a065aee084882d9c8a2dbd9ba34e9f839cd24188
Gillifish <drewkgilliland@gmail.com>
2023-10-19 20:31:26 -0400

    Initial commit

---
+++ src/main/java/org/gillinet/evermight/items/EvermightSwordItem.java
@@ -0,0 +1,11 @@
+package org.gillinet.evermight.items;
+
+import net.minecraft.world.item.SwordItem;
+import net.minecraft.world.item.Tier;
+
+public class EvermightSwordItem extends SwordItem {
+
+    public EvermightSwordItem(Tier p_43269_, int p_43270_, float p_43271_, Properties p_43272_) {
+        super(p_43269_, p_43270_, p_43271_, p_43272_);
+    }
+}
